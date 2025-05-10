c7b32b4f4a6b1d510a5b0d2e28b210f8e49da84f
itsmeayush26 <ayushsingh0255@gmail.com>
2025-05-10 17:50:28 +0530

    setup client and made Login & signup page

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+import Login from "./pages/Login"
+import './App.css'
+
+function App() {
+  return (
+    <main >
+      
+      <Login/>
+    </main>
+  )
+}
+
+export default App
---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+/* @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',                        // Make sure index.html is included
+    './src/**/*.{js,jsx,ts,tsx}',           // Scan all JS, JSX, TS, TSX files inside src
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: '#1da1f2',                   // Custom brand color
+      },
+    },
+  },
+  plugins: [],
+};
