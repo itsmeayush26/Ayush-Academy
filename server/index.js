213933db1a02edc178035150420de05ab335ba93
itsmeayush26 <ayushsingh0255@gmail.com>
2025-06-03 20:51:04 +0530

    added the frontend  render url

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ const PORT =process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://ayush-academy-frontend.onrender.com",
     credentials:true
 
 }))
